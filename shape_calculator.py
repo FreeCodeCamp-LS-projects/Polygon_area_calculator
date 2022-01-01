e1a733a8cb1e54641e32d7c19bbc8d57aeb0f4d8
Luis <96577528+ingluissantana@users.noreply.github.com>
2022-01-01 14:05:50 +0100

    Add files via upload

---
+++ shape_calculator.py
@@ -0,0 +1,54 @@
+class Rectangle:
+    
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+        
+    def __str__(self):
+        return f'Rectangle(width={self.width}, height={self.height})'
+    
+    def set_width(self,width):
+        self.width = width
+    
+    def set_height(self, height):
+        self.height = height
+    
+    def get_area(self):
+        area = self.width * self.height
+        return area
+        
+    def get_perimeter(self):
+        perimeter = (2 * self.width) + (2 * self.height)
+        return perimeter
+        
+    def get_diagonal(self):
+        diagonal = ((self.width ** 2 + self.height ** 2) ** .5)
+        return diagonal
+        
+    def get_picture(self):
+        i = self.width
+        j = self.height
+        rect_pic = ''
+        if i < 50 and j < 50: 
+            for x in range(j):
+                for y in range(i):
+                    rect_pic += '*'
+                rect_pic += '\n'
+        else:
+            rect_pic = "Too big for picture."
+        return rect_pic
+            
+        
+    def get_amount_inside(self, shape):
+        return self.get_area() // shape.get_area()
+        
+class Square(Rectangle):
+    def __init__(self,lenght):
+        super().__init__(width=lenght,height=lenght)
+        
+    def __str__(self):
+        return f'Square(side={self.width})'
+        
+    def set_side(self,lenght):
+        self.width = lenght
+        self.height = lenght
